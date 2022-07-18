4e0b14e764bfed6c5dd9ce321b1d85e595bb4b1d
FARDIN98 <fardinahmed66@gmail.com>
2022-07-18 16:26:02 +0600

    ..

--- Lab 5/linked_list.cpp
+++ Lab 5/linked_list.cpp
@@ -1,111 +1,120 @@
 #include<stdio.h>
 
-struct Node{
+struct Node
+{
     int data;
-
     struct Node *next;
 };
 typedef struct Node node;
 
+
 void menu();
 node* createNode();
 void listPrint(node *head);
+node* insert_head(node *head, node *nn);
+node* insert_tail(node *head, node *nn);
 
-
-
-int main(){
-    node *head = NULL:
+int main()
+{
+    node *head = NULL, *nn, *temp;
     int ch;
 
     menu();
-    printf("Enter choice: ");
-    scanf("%d", &ch);
+    printf("enter choice: ");
+    scanf("%d",&ch);
 
-    while (ch != 0)
+    while(ch != 0)
     {
-        if (ch == 1)
+        if(ch == 1)
         {
-            nn = create_node();
-            insert_head(head, nn);
+            nn = createNode();
+            head = insert_head(head, nn);
         }
-        else if (ch == 2)
+        else if(ch == 2)
         {
-            
-        }
-        else if (ch == 3)
-        {
-            nn = create_node();
 
         }
-        else if (ch == 8)
+        else if(ch == 3)
         {
-            
+            nn = createNode();
+            head = insert_tail(head, nn);
         }
-        else if (ch == 9)
+        else if(ch == 8)
         {
-            
+            listPrint(head);
         }
-        else{
-            printf("Invalid Input\n");
+        else if(ch == 9)
+        {
+
+        }
+        else
+        {
+            printf("wrong input.\n\n");
         }
         menu();
-        printf("Enter choice: ");
-        scanf("%d", &ch);
+        printf("enter choice: ");
+        scanf("%d",&ch);
     }
-    
+
 }
 
-menu(){
-    printf("1.Insert (head)\n");
-    printf("2.Insert (nth)\n");
-    printf("3.Insert (tail)\n");
-    printf("8.Print list\n");
-    printf("9.List Size\n");
-    printf("6.Exit\n");
+void menu()
+{
+    printf("1. insert(head)\n");
+    printf("2. insert(nth)\n");
+    printf("3. insert(tail)\n");
+    printf("8. print list\n");
+    printf("9. list size\n");
+    printf("0. exit\n");
 }
 
-node* create_node(){
+node* createNode()
+{
     node *nn = new node();
-    printf("Enter value: ");
-    scanf("%d", &nn->data);
+    printf("enter value: ");
+    scanf("%d",&nn->data);
     nn->next = NULL;
     return nn;
 }
 
-node* insert_head(node *head, node *nn){
-    if(head != NULL){
+node* insert_head(node *head, node *nn)
+{
+    if(head != NULL)
+    {
         nn->next = head;
     }
     head = nn;
     return head;
-    
 }
 
-void listPrint(node *head){
+void listPrint(node *head)
+{
     node *temp;
     temp = head;
-    while (temp != NULL)
+    while(temp != NULL)
     {
-        printf("%d ", temp->data);
-        temp  = temp->next;
+        printf("%d ",temp->data);
+        temp = temp->next;
     }
     printf("\n\n");
-    
 }
 
-
-
-node* insert_tail(node *head, node *nn){
-    if(head == NULL){
+node* insert_tail(node *head, node *nn)
+{
+    if(head == NULL)
+    {
         head = insert_head(head, nn);
     }
-    else{
+    else
+    {
         node *temp = head;
-        while (temp->next != NULL)
+
+        while(temp->next != NULL)
         {
             temp = temp->next;
         }
         temp->next = nn;
     }
+
     return head;
 }
